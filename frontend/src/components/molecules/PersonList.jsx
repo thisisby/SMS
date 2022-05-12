a0aeb369d9ef6ee07758df021ba0025f996e27e1
baistan.tashkuov <baistan.xi@gmail.com>
2022-05-12 17:16:49 +0600

    [feat] add person to personStatus

--- frontend/src/components/molecules/PersonList.jsx
+++ frontend/src/components/molecules/PersonList.jsx
@@ -1,26 +1,54 @@
 import React, { useContext } from "react";
 
 import { State } from "../../store/state";
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 import * as api from "../../store/api";
 
 const PersonList = () => {
   const { data } = useQuery("person", api.getPerson);
-  const { showMenu } = useContext(State);
+  const { mutate, isSuccess } = useMutation(api.createPersonStatus);
+
+  const { showMenu, showMenuHandler } = useContext(State);
+
+  const AddPersonStatus = (person) => {
+    console.log(person);
+    let date = new Date();
+    let options = {
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    let currentTime = date.toLocaleTimeString("en-us", options);
+    const newPersonStatus = {
+      ...person,
+      come: "",
+      leave: currentTime,
+      status: "On",
+    };
+    mutate(newPersonStatus);
+  };
   return (
     <div
-      className={`w-[20rem] bg-white duration-500 ease-in-out absolute top-0 ${
+      className={`w-[20rem] bg-white duration-500 ease-in-out fixed top-0 shadow-md ${
         showMenu ? "left-0" : "-left-full"
       } h-screen`}
     >
+      <button
+        onClick={showMenuHandler}
+        className="absolute top-7 right-8 sm:hidden"
+      >
+        X
+      </button>
       <h1 className="mb-10 py-6 px-5 text-xl">Select a student</h1>
       <div>
-        {data?.map((d) => (
+        {data?.map((person) => (
           <button
-            key={d.id}
+            onClick={() => AddPersonStatus(person)}
+            key={person.id}
             className="block w-full text-left px-4 py-4 duration-300 hover:bg-[#f6f6f6] capitalize"
           >
-            {d.id + ". " + d.full_name}
+            {person.id + ". " + person.full_name}
           </button>
         ))}
       </div>
--- frontend/src/store/api.js
+++ frontend/src/store/api.js
@@ -4,6 +4,13 @@ const api = axios.create({
   baseURL: "http://localhost:8000",
 });
 
+// Person
 export const getPerson = () => {
   return api.get("/person").then((res) => res.data);
+};
+
+// Person Status
+export const createPersonStatus = (person) => {
+  console.log(person);
+  api.post("/personStatus", person).then((res) => res.data);
 };
